5936386d163575d4aa24325c1608e3220e250908
艾帕 <elppa.chengp@alibaba-inc.com>
2017-12-07 10:45:17 +0800

    temp: init
    初始化

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # video
+
+系统要求： windows 10下使用
+
+chrome: 使用默认安装路径 
+
+将index.js下载到你的目录，并将你的mp4复制到这个目录命名为movie.mp4 
+
+运行命令
+
+```
+node 你的目录/index.js
+```
---
+++ index.js
@@ -0,0 +1,62 @@
+var fs = require("fs"),
+    http = require("http"),
+    url = require("url"),
+    path = require("path");
+
+var cp = require('child_process');
+var ls = cp.exec(`"c:\\program files (x86)\\Google\\Chrome\\Application\\chrome.exe" --start-fullscreen https://test.alibaba-inc.com/project/render-file/@ali/translate-demo/setting.html?v=http://localhost:8888/movie.mp4`, {}/*options, [optional]*/);
+
+ls.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+});
+
+ls.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+});
+
+ls.on('exit', function (code) {
+    console.log('child process exited with code ' + code);
+});
+
+http.createServer(function (req, res) {
+    if (req.url != "/movie.mp4") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end('<video src="http://localhost:8888/movie.mp4" controls></video>');
+    } else {
+        var file = path.resolve(__dirname, "movie.mp4");
+        fs.stat(file, function (err, stats) {
+            if (err) {
+                if (err.code === 'ENOENT') {
+                    // 404 Error if file not found
+                    return res.sendStatus(404);
+                }
+                res.end(err);
+            }
+            var range = req.headers.range;
+            if (!range) {
+                // 416 Wrong range
+                return res.sendStatus(416);
+            }
+            var positions = range.replace(/bytes=/, "").split("-");
+            var start = parseInt(positions[0], 10);
+            var total = stats.size;
+            var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+            var chunksize = (end - start) + 1;
+
+            res.writeHead(206, {
+                "Content-Range": "bytes " + start + "-" + end + "/" + total,
+                "Accept-Ranges": "bytes",
+                "Content-Length": chunksize,
+                "Content-Type": "video/mp4"
+            });
+
+            var stream = fs.createReadStream(file, { start: start, end: end })
+                .on("open", function () {
+                    stream.pipe(res);
+                }).on("error", function (err) {
+                    res.end(err);
+                });
+        });
+    }
+}).listen(8888);
+
